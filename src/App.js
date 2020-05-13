bff910ec23a4f121737f5f794f946a8be5812a59
Wayne Norman <wayne.jt.norman@gmail.com>
2020-05-13 15:24:30 -0400

    created homepage and added some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import MenuItem from './components/menu-item/menu-item.component';
 
 function App() {
   return <div>
     <HomePage/>
+    {/* <MenuItem/> */}
   </div>;
 }
 
